c6b77b94ad1015d2f68c0cda4ea36a7fef47a879
Jeff Papiez <jpapiez@microsoft.com>
2023-10-16 20:11:33 -0700

    updating after merging eith 303

--- autocommit.sh
+++ autocommit.sh
@@ -27,9 +27,6 @@ moonraker_folder=~/moonraker
 ### Path to your Mainsail folder, by default that is '~/mainsail'
 mainsail_folder=~/mainsail
 
-### Path to your Fluidd folder, by default that is '~/fluidd'
-#fluidd_folder=~/fluidd
-
 ### The branch of the repository that you want to save your config
 ### By default that is 'main'
 branch=main
